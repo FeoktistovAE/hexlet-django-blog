6b5815f160e21fe451b599270e18468af2298162
root <root@DESKTOP-6RAUT71.localdomain>
2022-12-28 01:11:27 +0300

    class based views practice

--- hexlet_django_blog/views.py
+++ hexlet_django_blog/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from django.views import View
 
 
-def index(request):
-    return render(request, 'index.html', context={
-        'who': 'World',
-    })
+class IndexView(View):
+    def get(self, request):
+        return render(request, 'index.html', context={
+            'who': 'Django',
+        })
 
 
 def about(request):
