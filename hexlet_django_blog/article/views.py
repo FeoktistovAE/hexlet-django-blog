fc97a565cb8cf5286cd77ee308bbea2fd23700b6
root <root@DESKTOP-6RAUT71.localdomain>
2022-12-28 22:42:18 +0300

    practiced reverseand redirect functions

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from django.urls import reverse
 
 # Create your views here.
 class IndexView(View):
-    def get(self, request):
-        tags = 'Articles page'
-        return render(request, 'article/index.html', context={'tags': tags})
+    def get(self, request, tags='', article_id = 0):
+        return render(request, 'article/index.html', context={'article_id': article_id, 'tags': tags})
+
