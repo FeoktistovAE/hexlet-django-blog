201edd7cab657177f6fbd6c404a00dc1218142cd
root <root@DESKTOP-6RAUT71.localdomain>
2023-01-07 18:13:08 +0300

    created Update page and flash messages

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 from hexlet_django_blog.article.models import Article
 from .forms import ArticleForm
+from django.contrib import messages
 
 
 # Create your views here.
@@ -33,5 +34,27 @@ class ArticleFormCreateView(View):
         form = ArticleForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.add_message(request, messages.SUCCESS, 'Article succesfully created')
             return redirect('articles')
+        messages.add_message(request, messages.ERROR, "Something went wrong")
         return render(request, 'article/create.html', {'form': form})
+    
+
+class ArticleFormEditView(View):
+
+    def get(self, request, *args, **kwargs):
+        article_id = kwargs.get('id')
+        article = Article.objects.get(id=article_id)
+        form = ArticleForm(instance=article)
+        return render(request, 'article/update.html', {'form': form, 'article_id': article_id})
+    
+    def post(self, request, *args, **kwargs):
+        article_id = kwargs.get('id')
+        article = Article.objects.get(id=article_id)
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            messages.add_message(request, messages.SUCCESS, 'Article succesfully updated')
+            return redirect('articles')
+        messages.add_message(request, messages.ERROR, "Something went wrong")
+        return render(request, 'article/update.html', {'form': form, 'article_id': article_id})
